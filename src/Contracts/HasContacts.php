d70f17850beb0547ceb04fc604fc6ae4b5e14f55
Kairat Jenishev <kairat.jenishev@gmail.com>
2026-02-25 01:13:34 +0600

    Add PHPDoc property and mixin for enhanced type support

--- src/Contracts/HasContacts.php
+++ src/Contracts/HasContacts.php
@@ -2,13 +2,18 @@
 
 namespace Jenishev\Laravel\Contacts\Contracts;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Collection;
 
 /**
  * Interface for models that can have contact information.
  *
  * Implement this interface on any model that should support polymorphic
  * contact records (phone, email, address, etc.).
+ *
+ * @mixin Model
+ * @property-read Collection $contacts
  */
 interface HasContacts
 {
